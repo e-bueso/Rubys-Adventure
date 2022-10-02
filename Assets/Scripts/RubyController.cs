6e9d554074c8a15e0ebd205d9e71835b52a7adca
e-bueso <bueso@uji.es>
2022-10-02 11:35:40 +0200

    v0.2

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,24 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log("FrameRate: " + (1.0f / Time.deltaTime));
+       // QualitySettings.vSyncCount = 0;
+       // Application.targetFrameRate = 24;        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;        
+
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       // Debug.Log(horizontal);
+
+       // deltaTime, contained inside Time, is a variable that Unity fills with the time it takes for a frame to be rendered
+       Vector2 position = transform.position;
+       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+       position.y = position.y + 3.0f * vertical * Time.deltaTime;
+       transform.position = position;
+
     }
 }
