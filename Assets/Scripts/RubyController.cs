b0f47c3f076cd6f3c8696a898211250db85fdf27
e-bueso <bueso@uji.es>
2022-10-03 01:05:21 +0200

    0.6

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal; 
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+       rigidbody2d = GetComponent<Rigidbody2D>();
+       
        Debug.Log("FrameRate: " + (1.0f / Time.deltaTime));
        // QualitySettings.vSyncCount = 0;
        // Application.targetFrameRate = 24;        
@@ -16,15 +22,18 @@ public class RubyController : MonoBehaviour
     void Update()
     {
 
-       float horizontal = Input.GetAxis("Horizontal");
-       float vertical = Input.GetAxis("Vertical");
+       horizontal = Input.GetAxis("Horizontal");
+       vertical = Input.GetAxis("Vertical");
        // Debug.Log(horizontal);
 
-       // deltaTime, contained inside Time, is a variable that Unity fills with the time it takes for a frame to be rendered
-       Vector2 position = transform.position;
+    }
+    void FixedUpdate() {
+       Vector2 position = rigidbody2d.position;
+ 
+        // deltaTime, contained inside Time, is a variable that Unity fills with the time it takes for a frame to be rendered
        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-       transform.position = position;
-
+       
+       rigidbody2d.MovePosition(position);
     }
 }
