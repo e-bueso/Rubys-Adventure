b30afb93e750b44fae966c2d73dd240848e7252e
e-bueso <bueso@uji.es>
2022-10-03 14:49:58 +0200

    v7.0

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,10 +8,18 @@ public class RubyController : MonoBehaviour
     float horizontal; 
     float vertical;
 
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
+    public int health { get { return currentHealth; }}
+
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
+
+       currentHealth = maxHealth;
+       currentHealth = 1;
        
        Debug.Log("FrameRate: " + (1.0f / Time.deltaTime));
        // QualitySettings.vSyncCount = 0;
@@ -31,9 +39,16 @@ public class RubyController : MonoBehaviour
        Vector2 position = rigidbody2d.position;
  
         // deltaTime, contained inside Time, is a variable that Unity fills with the time it takes for a frame to be rendered
-       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-       position.y = position.y + 3.0f * vertical * Time.deltaTime;
+       position.x = position.x + speed * horizontal * Time.deltaTime;
+       position.y = position.y + speed * vertical * Time.deltaTime;
        
        rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
